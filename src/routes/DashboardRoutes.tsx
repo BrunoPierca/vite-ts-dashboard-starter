f4e95cf2d8e0e553070dddb1f2a687f59782efd8
BrunoPierca <pierca18@gmail.com>
2023-09-30 02:11:07 -0300

    removed unused dash routes

--- src/routes/DashboardRoutes.tsx
+++ src/routes/DashboardRoutes.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { Home } from '../pages';
+import { Home, Users } from '../pages';
 
 // import Navbar from '../common/components/Navbar/Navbar';
 import { DashboardLayout } from '../common/layout';
@@ -13,13 +13,7 @@ export const DashboardRoutes = () => {
             <DashboardLayout>
                 <Routes>
                     <Route path='home' element={<Home />} />
-                    {/* <Route path='jobs' element={<Jobs />} />
-                    <Route path='workers' element={<Workers />} />
-                    <Route path='companies' element={<Companies />} />
                     <Route path='users' element={<Users />} />
-                    <Route path='applicants' element={<Applicants />} />
-                    <Route path='received-payments' element={<ReceivedPayments />} />
-                    <Route path='payments' element={<Payments />} /> */}
                     <Route path='/*' element={<Navigate to="/home" />} />
                 </Routes>
 
