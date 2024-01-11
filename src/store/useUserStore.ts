d7803f8776f78faac06938f29319b42e2c87bf41
BrunoPierca <pierca18@gmail.com>
2024-01-11 11:40:23 -0300

    Updated user's store actions

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -2,18 +2,20 @@ import { create } from "zustand";
 import { User } from "../interfaces";
 
 // Check if delete is useful, remove if not.
-type actions = 'create' | 'edit' | 'details' | 'delete'
+export type Action = 'create' | 'edit' | 'details' | 'delete'
 
 interface UsersState {
     activeUser: User | null;
-    setActiveUser: (user: User, action: actions) => void;
-    modalAction: actions | null;
+    setActiveUser: (user: User, action: Action) => void;
+    modalAction: Action | null;
     removeActiveUser: () => void;
+    setModalAction: (action: Action) => void;
 }
 
 export const useUsersStore = create<UsersState>((set) => ({
     activeUser: null,
-    setActiveUser: (user: User | null, action: actions) => set((state) => ({ ...state, activeUser: user, modalAction: action })),
+    setActiveUser: (user: User | null, action: Action) => set((state) => ({ ...state, activeUser: user, modalAction: action })),
     modalAction: null,
-    removeActiveUser: () => set((state) => ({ ...state, activeUser: null, modalAction: null }))
+    removeActiveUser: () => set((state) => ({ ...state, activeUser: null, modalAction: null })),
+    setModalAction: (action: Action) => set((state) => ({ ...state, modalAction: action })),
 }));
